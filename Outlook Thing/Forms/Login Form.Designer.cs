708b55c041f5f20aa9c047b6ffa1b304ba84ca86
Donovan Gilliam <donovangilliam45@gmail.com>
2024-08-22 14:46:40 -0400

    json

--- Outlook Thing/Forms/Login Form.Designer.cs
+++ Outlook Thing/Forms/Login Form.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.client_id_label = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // client_id_label
+            // 
+            this.client_id_label.AutoSize = true;
+            this.client_id_label.Location = new System.Drawing.Point(107, 125);
+            this.client_id_label.Name = "client_id_label";
+            this.client_id_label.Size = new System.Drawing.Size(17, 16);
+            this.client_id_label.TabIndex = 0;
+            this.client_id_label.Text = "\"\"";
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 566);
+            this.Controls.Add(this.client_id_label);
             this.Name = "LoginForm";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.LoginForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label client_id_label;
     }
 }
 
--- Outlook Thing/Program.cs
+++ Outlook Thing/Program.cs
@@ -1,24 +1,52 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using Outlook_Calendar_Manager.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.Json;
+using System.IO;
+using Microsoft.Identity.Client;
 
 namespace Outlook_Calendar_Manager
 {
+    //thanks to cmastkas.com
     internal static class Program
     {
-        private static string clientID;
-        private static string tenant;
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        public static string CLIENT_ID;
+        public static string TENANT_ID;
+
+        private static IPublicClientApplication clientApp;
+
         [STAThread]
         static void Main()
         {
+            LoadInfo();
+            InitAuth();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
+        }
+        public static IPublicClientApplication PublicClientApp { get { return clientApp; } }
+        public static void LoadInfo()
+        {
+            string relPath = "Data/AuthInfo.json";
+            string absPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relPath);
+            string json = File.ReadAllText(absPath);
+            using (JsonDocument jsondoc = JsonDocument.Parse(json)) 
+            {
+                JsonElement root = jsondoc.RootElement;
+                CLIENT_ID = root.GetProperty("clientId").ToString();
+                TENANT_ID = root.GetProperty("tenantId").ToString();
+            }
+        }
+        public static void InitAuth()
+        {
+            clientApp = PublicClientApplicationBuilder.Create(CLIENT_ID)
+                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                .WithAuthority(AzureCloudInstance.AzurePublic, TENANT_ID)
+                .Build();
         }
     }
 }
